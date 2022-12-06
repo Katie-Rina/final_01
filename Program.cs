c18627a7b6d5074b6662e49c0cb39feba2ae1cbb
KatieRina <eashchukina@yandex.ru>
2022-12-07 01:02:26 +0300

    write program

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите элементы массива через пробел: ");
+string? inserted = Console.ReadLine();
+string[] elements = inserted.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+GetAndPrintResult(elements);
+
+void GetAndPrintResult(string[] elements)
+{
+    string[] resMass = new string [elements.GetLength(0)];
+    int j = 0;
+    for (int i = 0; i < elements.GetLength(0); i++)
+    {
+        if (elements[i].Length < 4)
+        {
+            resMass[j] = elements[i];
+            Console.Write($"{resMass[j]} ");
+            j++;
+        }
+    }
+}
+
